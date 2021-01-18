72440ac2c3f5b0ddf51c4a14aee4066e06abfc90
rajatplease <itsrajatkaushik@gmail.com>
2021-01-18 19:49:03 +0530

    ready to push

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Add } from "./components/Add";
+import { Watched } from "./components/Watched";
+import "./lib/font-awesome/css/all.min.css";
+
+import { GlobalProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Watchlist />
+          </Route>
+          <Route path="/watched">
+            <Watched />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+        </Switch>
+      </Router>
+    </GlobalProvider>
+  );
+}
+
+export default App;
